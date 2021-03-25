23634fbdef49a45a2651b27b62113cd587f6d5b3
Natallia Yurcha <natallia.yurcha@gmail.com>
2021-03-25 20:13:43 +0300

    feat: implement createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arrayOfName) {
+  if (arrayOfName === null || arrayOfName === undefined) {
+    return false;
+  } else {
+    let firstLettersOfNamesArray = [];
+    for (let i = 0; i < arrayOfName.length; i++) {
+      if (typeof arrayOfName[i] === 'string' && arrayOfName[i] !== null) {
+        let nameWithoutSpaces = arrayOfName[i].trim();
+        let firstLetterOfCurrentName = nameWithoutSpaces[0].toUpperCase();
+        firstLettersOfNamesArray.push(firstLetterOfCurrentName);
+      }
+    }
+    let teamNameSort = firstLettersOfNamesArray.sort();
+    console.log(teamNameSort);
+    let teamName = firstLettersOfNamesArray.join('');
+    return teamName;
+  }
 };
